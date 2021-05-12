c9a511d9144321635d0502e09bdcd25184c2b800
DESKTOP-FUPSOE7\TRANQUANGTRUNG <tqtrungse@gmail.com>
2021-05-12 12:42:14 +0700

    chg(message): define constructor for array

--- include/message/array.h
+++ include/message/array.h
@@ -9,7 +9,15 @@ struct Array {
     std::shared_ptr<T> ptr;
     uint16_t length;
 
-    Array() : ptr(nullptr), length(0) {}
+    Array() 
+        : ptr(nullptr), 
+          length(0) {}
+          
+    Array(T* ptr, uint16_t length) 
+        : ptr(ptr), 
+          length(length) {}
+
+    Array(const Array<T>& other) = default;
 };
 
 #endif //_MESSAGE_ARRAY_H_
