5461d2946ade7c5deab03d2835a233f8e7213afd
DESKTOP-FUPSOE7\TRANQUANGTRUNG <tqtrungse@gmail.com>
2021-05-15 11:48:58 +0700

    chg(utils): refactor code

--- include/utils/utils_concurrency_queue.h
+++ include/utils/utils_concurrency_queue.h
@@ -11,7 +11,7 @@
 #include "utils_safe.h"
 #include "utils_spin_lock.h"
 
-// "Queue" is general class for any type.
+// "ConcurrencyQueue" is general class for any type.
 // Except std::unique_ptr (because it doesn't has copy constructor)
 // and raw pointer (can implement but not necessary yet)
 template<typename ValueType>
@@ -28,28 +28,6 @@ private:
     uint32_t index_read;
     uint32_t index_write;
 
-private:
-    #define SYNC_READ(index, ret)                                   \
-    if (this->size.fetch_sub(1) <= 0) {                             \
-        this->size.fetch_add(1);                                    \
-        ret.first = Error::Empty;                                   \
-        return ret;                                                 \
-    }                                                               \
-    this->spin.lock();                                              \
-    index = this->index_read;                                       \
-    this->index_read = (this->index_read + 1) % this->capacity;     \
-    this->spin.unlock();
-
-    #define SYNC_WRITE(index)                                       \
-    if (this->size.fetch_add(1) >= this->capacity) {                \
-        this->size.fetch_sub(1);                                    \
-        return Error::Full;                                         \
-    }                                                               \
-    this->spin.lock();                                              \
-    index = this->index_write;                                      \
-    this->index_write = (this->index_write + 1) % this->capacity;   \
-    this->spin.unlock();
-
 public:
     ConcurrencyQueue(uint32_t capacity) 
         : spin(), 
@@ -60,7 +38,7 @@ public:
         assert(this->capacity > 0);
         this->buffer = Safe::new_arr<storage>(this->capacity);
         if (this->buffer == nullptr) {
-            throw "[utils_concurrency_queue/ConcurrencyQueue(uin32_t capacity)]Not enough memory to create array";
+            throw std::runtime_error("[utils_concurrency_queue/ConcurrencyQueue(uin32_t capacity)]Not enough memory to create array");
         }
     }
 
@@ -71,10 +49,10 @@ public:
     void clear() {
         // "fetch_add" or "fetch_sub" uses "std::memory_order_seq_cst"
         this->size.store(0, std::memory_order_seq_cst);
-        LOCK(&this->spinLock);
+        this->spin.lock();
         this->index_read = 0;
         this->index_write = 0;
-        UNLOCK(&this->spinLock);
+        this->spin.unlock();
     }
 
     Error::Code push(ValueType&& value) {
@@ -82,39 +60,42 @@ public:
     }
 
     Error::Code push(ValueType& value) {
-        uint32_t index;
-        SYNC_WRITE(index)
+        if (this->size.fetch_add(1) >= this->capacity) {
+            this->size.fetch_sub(1);
+            return Error::Full;
+        }
+
+        // Use "SpinLock" to guard in case multiple threads call this function together
+        this->spin.lock();
+        uint32_t index = this->index_write;
+        this->index_write = (this->index_write + 1) % this->capacity;
+        this->spin.unlock();
         // "std::addressof" prevents user wrong implementation "operator &"
         // User can return address of some fields of object, not object's address
         new (static_cast<void*>(std::addressof(this->buffer[index])))ValueType(value);
         return Error::Nil;
     }
 
-    // If "ValueType" is not std::share_ptr<T> =, this function will be complied
-    template <typename T, 
-              bool IsSharePtr = std::is_same<ValueType, std::shared_ptr<T>>::value, 
-              typename std::enable_if<!IsSharePtr>::type* = nullptr>
     std::pair<Error::Code, ValueType> pop() {
-        uint32_t index;
-        std::pair<Error::Code, ValueType> ret;
-        SYNC_READ(index, ret)
-        ValueType* ptr = reinterpret_cast<ValueType*>(std::addressof(this->buffer[index]));
-        std::swap(ret.second, *ptr);
-        return ret;
-    }
+        std::pair<Error::Code, ValueType> ret(Error::Nil, ValueType());
+        // Check empty
+        if (this->size.fetch_sub(1) <= 0) {
+            this->size.fetch_add(1);
+            ret.first = Error::Empty;
+            return ret;
+        }
 
-    // If "ValueType" is std::share_ptr<T> =, this function will be complied
-    // Must specify the argument "std::shared_ptr" to update the "nullptr" for 
-    // the queue's word after "pop" to completely transfer ownership of the 
-    // memory to the user.
-    template <typename T, 
-              bool IsSharePtr = std::is_same<ValueType, std::shared_ptr<T>>::value,
-              typename std::enable_if<IsSharePtr>::type* = nullptr>
-    std::pair<Error::Code, ValueType> pop() {
-        uint32_t index;
-        std::pair<Error::Code, ValueType> ret;
-        SYNC_READ(index, ret)
-        ret.second.swap(reinterpret_cast<ValueType&>(this->buffer[index]));
+        // Use "SpinLock" to guard in case multiple threads call this function together
+        this->spin.lock();
+        uint32_t index = this->index_read;
+        this->index_read = (this->index_read + 1) % this->capacity;
+        this->spin.unlock();
+
+        // Get value
+        ValueType* ptr = reinterpret_cast<ValueType*>(std::addressof(this->buffer[index]));
+        // Use "std::swap" completely transfer ownership of the memory to the user
+        // if "ValueType" is "std::share_ptr" or struct containing "std :: share_ptr" fields
+        std::swap(ret.second, *ptr);
         return ret;
     }
 };
