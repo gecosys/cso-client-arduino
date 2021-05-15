b004a03f5b65ccd73a56dd48c4d96ebbf3d1e685
DESKTOP-FUPSOE7\TRANQUANGTRUNG <tqtrungse@gmail.com>
2021-05-15 11:48:41 +0700

    chg(utils): update utils code

--- include/utils/utils_code.h
+++ include/utils/utils_code.h
@@ -5,20 +5,21 @@
 
 class Error {
 public:
-    enum Code : uint8_t {
-        Nil = 0,
-        Full,
-        Empty,
-        Other,
-        Build,
-        Parse,
-        Verify,
-        Encrypt,
-        Decrypt,
-        HttpError,
-        NotReady,
-        NotEnoughMem,
-        NotConnectServer,
+    enum Code : int8_t {
+        Nil                 = 0,
+        Full                = -1,
+        Empty               = -2,
+        Other               = -3,
+        Build               = -4,
+        Parse               = -5,
+        Verify              = -6,
+        Encrypt             = -7,
+        Decrypt             = -8,
+        HttpError           = -9,
+        NotReady            = -10,
+        NotEnoughMem        = -11,
+        NotConnectServer    = -12,
+        RespEmpty           = -13,
     };
 };
 
