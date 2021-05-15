815572ac60573403cb065640ed317b4323d4bb3d
DESKTOP-FUPSOE7\TRANQUANGTRUNG <tqtrungse@gmail.com>
2021-05-15 11:47:48 +0700

    chg(proxy): fix BigNumber's crashed when using default constructor in return

--- include/cso_proxy/message.h
+++ include/cso_proxy/message.h
@@ -7,18 +7,20 @@
 
 // ServerKey is a group of server keys
 struct ServerKey {
+    // "BigNumber" has to set default data unless when returning will be crashed
     BigNumber gKey;
     BigNumber nKey;
     BigNumber pubKey;
 
-    ServerKey() = default;
+    ServerKey() 
+        : gKey("-1"), nKey("-1"), pubKey("-1") {}
     ServerKey(const char* gKey, const char* nKey, const char* pubKey)
         : gKey(gKey), nKey(nKey), pubKey(pubKey) {}
 };
 
 // ServerTicket is an activation ticket from the Hub server
 struct ServerTicket {
-    String hubAddress;
+    std::string hubAddress;
     uint16_t ticketID;
     std::shared_ptr<byte> ticketBytes;
     std::shared_ptr<byte> serverSecretKey;
